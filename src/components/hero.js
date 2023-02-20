c38fbdce953d8ae8e5173d2e79a4bb9b03d1f418
Lisa Lee Kallman <59650403+leekallman@users.noreply.github.com>
2023-02-20 12:56:28 +0100

    Update hero.js

--- src/components/hero.js
+++ src/components/hero.js
@@ -10,10 +10,10 @@ const Hero = () => {
                 <h1 className="heroHeading">Lisa Lee</h1>
                 <div className="heroDescription">
                     I’m a <div className="heroAnimation" onMouseEnter={() => cursorChangeHandler("hovered")}
-                        onMouseLeave={() => cursorChangeHandler("")}><div className="static">Frontend Developer</div><div className="dynamic"><p>Frontend Developer</p></div></div> and creative coder based in Stockholm.
-                    I’m a current <div className="heroAnimation" onMouseEnter={() => cursorChangeHandler("hovered")}
-                        onMouseLeave={() => cursorChangeHandler("")}><div className="static">Hyper Island student</div><div className="dynamic"><p>Hyper Island student</p></div></div> with freelance experience and a former <div className="heroAnimation" onMouseEnter={() => cursorChangeHandler("hovered")}
-                            onMouseLeave={() => cursorChangeHandler("")}><span className="static">Project Manager</span><span className="dynamic"><p>Project Manager</p></span></div> in the fields of civil engineering and architecture.</div>
+                        onMouseLeave={() => cursorChangeHandler("")}><div className="static">Digital Designer</div><div className="dynamic"><p>Digital Designer</p></div></div> and Creative Developer based in Stockholm.
+                    I’m a <div className="heroAnimation" onMouseEnter={() => cursorChangeHandler("hovered")}
+                        onMouseLeave={() => cursorChangeHandler("")}><div className="static">Hyper Island alumni</div><div className="dynamic"><p>Hyper Island alumni</p></div></div> with freelance experience and several years of experience as<div className="heroAnimation" onMouseEnter={() => cursorChangeHandler("hovered")}
+                            onMouseLeave={() => cursorChangeHandler("")}><span className="static">Project Manager</span><span className="dynamic"><p>Project Manager</p></span></div> in the fields of Civil Engineering and Architecture.</div>
             </div>
         </div>
     );
